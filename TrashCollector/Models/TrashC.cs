a4d288ebaf42451d2cf1df24c11b13e97f9ab338
gjjuliex <12baysidejgjx@gmail.com>
2018-10-29 13:05:41 -0500

    finally got the sign up page for employee/user

---
+++ TrashCollector/Models/TrashC.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class TrashC
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public int Zipcode { get; set; }
+        public DateTime Date { get; set; }
+        public string Days { get; set; }
+        public double Bill { get; set; }
+        [Display(Name = "Start Date")]
+        public DateTime StartDate { get; set; }
+        [Display(Name = "End Date")]
+        public DateTime EndDate { get; set; }
+        public int Address { get; set; }
+           
+    }
+}
